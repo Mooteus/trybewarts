acf271efe7e08e564bd7e0fc239df274f46eb5b7
araujo-marcio <araujo.marcio@pm.me>
2022-02-26 21:48:58 -0300

    Entregando 21º Requisito

--- script.js
+++ script.js
@@ -29,3 +29,64 @@ agreement.addEventListener('change', () => {
     buttonCheck.disabled = true;
   }
 });
+
+function generateParagraph(text) {
+  const paragraph = document.createElement('p');
+  const infoContainer = document.getElementById('info-container');
+  paragraph.innerText = text;
+  infoContainer.appendChild(paragraph);
+}
+
+function getFamily() {
+  const frontEnd = document.getElementById('frontend');
+  const backEnd = document.getElementById('backend');
+  const fullStack = document.getElementById('fullstack');
+
+  if (frontEnd.checked) {
+    return frontEnd.value;
+  } if (backEnd.checked) {
+    return backEnd.value;
+  } if (fullStack.checked) {
+    return fullStack.value;
+  }
+}
+
+function getTechs() {
+  const subjects = document.querySelectorAll('.subject');
+  const content = [];
+  for (let index = 0; index < subjects.length; index += 1) {
+    if (subjects[index].checked) {
+      content.push(subjects[index].value);
+    }
+  }
+  return content.join(', ');
+}
+
+function getRate() {
+  const rate = document.querySelectorAll('.rate');
+
+  for (let index = 0; index < rate.length; index += 1) {
+    if (rate[index].checked) {
+      return rate[index].value;
+    }
+  }
+}
+const form = document.getElementById('evaluation-form');
+
+form.addEventListener('submit', () => {
+  const name = document.getElementById('input-name').value;
+  const lastName = document.getElementById('input-lastname').value;
+  const email = document.getElementById('input-email').value;
+  const house = document.getElementById('house').value;
+  const family = getFamily();
+  const techs = getTechs();
+  const rate = getRate();
+  const textArea = document.getElementById('textarea').value;
+  generateParagraph(`Nome: ${name} ${lastName}`);
+  generateParagraph(`Email: ${email}`);
+  generateParagraph(`Casa: ${house}`);
+  generateParagraph(`Família: ${family}`);
+  generateParagraph(`Matérias: ${techs}`);
+  generateParagraph(`Avaliação: ${rate}`);
+  generateParagraph(`Observações: ${textArea}`);
+});
